d9050109971989b045b90dfb492bbba3e10fe2e3
chartung <chartung@seas.upenn.edu>
2020-08-24 12:44:15 -0700

    Updates

---
+++ precache-manifest.7276fdd00fcb9954e7e869e6acd82028.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26b5768740893d94acd6ff0ea42f4b24",
+    "url": "/quote-generator/index.html"
+  },
+  {
+    "revision": "3b3045fcbe6a9f624d4f",
+    "url": "/quote-generator/static/css/main.40006832.chunk.css"
+  },
+  {
+    "revision": "47d48c4b689164651ca7",
+    "url": "/quote-generator/static/js/2.f9e7299b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quote-generator/static/js/2.f9e7299b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b3045fcbe6a9f624d4f",
+    "url": "/quote-generator/static/js/main.44c6b82c.chunk.js"
+  },
+  {
+    "revision": "6e71cb284c179124fe30",
+    "url": "/quote-generator/static/js/runtime-main.8faf9592.js"
+  }
+]);
